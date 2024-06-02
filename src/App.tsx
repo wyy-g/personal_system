369c283324752fa280740bf6d577786d92312e84
wyy-g <1482581329@qq.com>
2024-06-03 00:49:55 +0800

    feature: 初始化项目

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Test from "@/components/test";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Test />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/test.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Test: FC = () => {
+  return <div>123</div>;
+};
+
+export default Test;
